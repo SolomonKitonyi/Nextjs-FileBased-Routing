e2dff7921b64ba7391c4332ac239b6a05d8227ec
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-06 12:53:07 +0300

    access the query

--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const ProjectPage = () => {
+  const router = useRouter();
+  console.log(router.path);
+  console.log(router.query);
   return (
     <div>
       <h1>The Project Id Page</h1>
